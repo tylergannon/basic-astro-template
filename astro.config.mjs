171c729a956d0eef6e165193867818def95ba2e1
Tyler Gannon <gh@tylergannon.com>
2023-07-02 17:11:20 -0600

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig, sharpImageService } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://localhost:3000",
   experimental: {
     assets: true,
   },
   image: {
     service: sharpImageService(),
   },
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
