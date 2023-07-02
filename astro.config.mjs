98047693f85b4aaccf00630006f073123ac23929
Tyler Gannon <gh@tylergannon.com>
2023-07-02 15:07:51 -0600

    Basic config working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: sharpImageService()
+  },
+  integrations: [mdx()]
+});
