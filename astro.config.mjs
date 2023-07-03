f67b22551da3adcf927d1bed71fcea23ebb0a995
Tyler Gannon <gh@tylergannon.com>
2023-07-02 20:03:02 -0600

    Install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+// import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
